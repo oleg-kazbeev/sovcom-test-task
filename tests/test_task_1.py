a28540f1f237297293da067bb09cae413dabd698
Oleg <kazbeev@gmail.com>
2024-07-08 22:23:51 +0700

    Fixed way of process input data + output data format.

--- tests/test_task_1.py
+++ tests/test_task_1.py
@@ -9,35 +9,35 @@ class TestShareCalculator(unittest.TestCase):
     def setUp(self):
         self.calculator = ShareCalculator()
 
-    @patch('sys.argv', ['script_name.py', '4', '1.5', '3', '6', '1.5'])
+    @patch('sys.stdin', StringIO('4\n1.5\n3\n6\n1.5\n'))
     def test_process(self):
-        expected_output = "12.500\n25.000\n50.000\n12.500\n"
+        expected_output = "0.125\n0.250\n0.500\n0.125\n"
         with patch('sys.stdout', new=StringIO()) as fake_out:
             self.calculator.process()
             self.assertEqual(fake_out.getvalue(), expected_output)
 
-    @patch('sys.argv', ['script_name.py', '4', '2', '2', '2', '2'])
+    @patch('sys.stdin', StringIO('4\n2\n2\n2\n2\n'))
     def test_equal_shares(self):
-        expected_output = "25.000\n25.000\n25.000\n25.000\n"
+        expected_output = "0.250\n0.250\n0.250\n0.250\n"
         with patch('sys.stdout', new=StringIO()) as fake_out:
             self.calculator.process()
             self.assertEqual(fake_out.getvalue(), expected_output)
 
-    @patch('sys.argv', ['script_name.py', '2', '1', '4'])
+    @patch('sys.stdin', StringIO('2\n1\n4\n'))
     def test_unequal_shares(self):
-        expected_output = "20.000\n80.000\n"
+        expected_output = "0.200\n0.800\n"
         with patch('sys.stdout', new=StringIO()) as fake_out:
             self.calculator.process()
             self.assertEqual(fake_out.getvalue(), expected_output)
 
-    @patch('sys.argv', ['script_name.py', '3', '1', '1', '1'])
+    @patch('sys.stdin', StringIO('3\n1\n1\n1\n'))
     def test_fractional_shares(self):
-        expected_output = "33.333\n33.333\n33.333\n"
+        expected_output = "0.333\n0.333\n0.333\n"
         with patch('sys.stdout', new=StringIO()) as fake_out:
             self.calculator.process()
             self.assertEqual(fake_out.getvalue(), expected_output)
 
-    @patch('sys.argv', ['script_name.py', '3', '1', '1', '1', '2'])
+    @patch('sys.stdin', StringIO('4\n1\n1\n1\n'))
     def test_wrong_shares_number(self):
         err_msg = "Number of shares provided does not match the specified N."
         with self.assertRaises(ValueError) as cm:
